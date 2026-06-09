0fd0e5b0d438e0097fb28bd63f29dd0e54e8a0a2
ANA ANSELMI <anaanselmi0@gmail.com>
2026-06-08 21:34:59 -0400

    fix: agregar vite config en fase 4 y recompilar distribuciones

--- fase03-producto-mvp/vite.config.js
+++ fase03-producto-mvp/vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'distrito'
-  }
+  base: './', // <-- Poner un punto y barra elimina el problema de las rutas absolutas en subcarpetas de GitHub Pages
 }) 
